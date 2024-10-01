143ac39ae81a2b7f19b1a54982f01d09653cf6a6
Rgugs <rgugs10@gmail.com>
2024-10-01 15:28:32 -0700

    added dictionary sort feature and generate report function

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 def main():
     book = r'books/frankenstein.txt'
     
+    # Read text into memory
     text = get_book_path(book)
+
+    # Count words
     count = count_words(text)
+
+    # Could combine these into single function to return a sorted dictionary 
     count_dict = count_characters(text)
-    
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{count} words found in the document")
-    print(count_dict)
+    sorted_dict = sort_dict(count_dict)
+
+    # Final output
+    generate_report(book, count, sorted_dict)
     
 def get_book_path(path):
     with open(path) as f:
@@ -20,15 +25,29 @@ def count_words(text):
 def count_characters(string):
     lowered = string.lower().split()
     count_dict = {}
-    character_list = []
     for i in lowered:
         for c in i:
-            if c in count_dict:
-                count_dict[c] += 1
-            else:
-                count_dict[c] = 1
-
+            if c.isalpha():
+                if c in count_dict:
+                    count_dict[c] += 1
+                else:
+                    count_dict[c] = 1
+    
     return count_dict
 
+def sort_dict(in_dict):
+    sorted_dict = dict(sorted(in_dict.items(), key=lambda item: item[1], reverse=True))
+
+    return sorted_dict
+
+def generate_report(book, count, sorted_dict):
+
+    print(f"--- Begin report of {book} ---")
+    print(f"{count} words found in the document")
+    print("")
+
+    for k, v in sorted_dict.items():
+        print(f"The '{k}' character was found {v} times")
+    print("--- End report ---")
 
 main()
